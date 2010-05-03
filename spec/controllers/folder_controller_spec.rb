95ff88dc6344cb8b53bda757aa6f79e8bfc94876
William Butler <tachyonwill@gmail.com>
2010-05-03 15:31:10 -0400

    folder_controller rspec test work

--- spec/controllers/folder_controller_spec.rb
+++ spec/controllers/folder_controller_spec.rb
@@ -1,17 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-require 'rubygems'
-require 'marc'
-
 describe FolderController do
+  before(:each) do
+    request.env["HTTP_REFERER"] = "/"
+  end
     it "should add items to list" do
-      params[:id] = "77826928"
-      get :create
-      params[:id] = "94120425"
-      get :create
-      session[:folder_document_ids].should include("77826928")
-      session[:folder_document_ids].should include("94120425")
+      get :create, :id =>"77826928"
+      session[:folder_document_ids].length.should == 1
+      get :create, :id => "94120425"
       session[:folder_document_ids].length.should == 2
+      session[:folder_document_ids].should include("77826928")
       get :index
-      @documents.length.should == 2
+      assigns[:documents].length.should == 2
+      assigns[:documents].first.should be_instance_of(SolrDocument)
+    end
+    it "should delete an item from list" do
+      get :create, :id =>"77826928"
+      get :create, :id => "94120425"
+      get :destroy, :id =>"77826928"
+      session[:folder_document_ids].length.should == 1
+    end
+    it "should clear list" do
+      get :create, :id =>"77826928"
+      get :create, :id => "94120425"
+      get :clear
+      session[:folder_document_ids].length.should == 0
     end
 end
